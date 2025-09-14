d28a0206637c8aba600c54b45b9e0fc7efb7be87
Ayaan <ayaan.waqas@outlook.com>
2025-09-14 17:32:56 +0100

    nixvim still ain't workin'

--- modules/homeManager/editors/nvim.nix
+++ modules/homeManager/editors/nvim.nix
@@ -4,4 +4,31 @@
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
   ];
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    plugins = with pkgs.vimPlugins; [
+      catppuccin-nvim
+    ];
+  };
+
+  programs.nixvim = {
+    enable = true;
+    defaultEditor = true;
+
+    colorschemes.catppuccin = {
+      enable = true;
+
+      settings = {
+        flavour = "macchiato";
+
+	transparent_background = true;
+      };
+    };
+  };
 }
