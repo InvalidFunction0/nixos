6d07aca602036382f224bd22b44eba8d713e5183
Ayaan <ayaan.waqas@outlook.com>
2025-09-16 12:14:13 +0100

    don't remember but they were important

--- modules/homeManager/editors/nvim.nix
+++ modules/homeManager/editors/nvim.nix
@@ -64,6 +64,7 @@
 
     plugins = {
       lualine.enable = true;
+      luasnip.enable = true;
       treesitter.enable = true;
       web-devicons.enable = true;
       colorizer.enable = true;
@@ -87,6 +88,45 @@
           { name = "nvim_lsp"; }
           { name = "treesitter"; }
         ];
+
+        setings.mapping = {
+          "<CR>" =
+            "cmp.mapping(function(fallback)
+              if cmp.visible() then
+                if luasnip.expandable() then
+                  luasnip.expand()
+                else
+                  cmp.confirm({
+                    select = true,
+                  })
+                end
+              else
+                fallback()
+              end
+            end)";
+
+          "<Tab>" =
+            "cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.locally_jumpable(1) then
+                luasnip.jump(1)
+              else
+                fallback()
+              end
+            end, { \"i\", \"s\" })";
+
+          "<S-Tab>" =
+            "cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { \"i\", \"s\" })";
+        };
       };
       
       comment = {
