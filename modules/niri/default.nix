9473d756680c7110f2da08c12a76e953a293a0f5
Ayaan <ayaan.waqas@outlook.com>
2025-12-14 20:37:30 +0000

    broken niri config

--- modules/niri/default.nix
+++ modules/niri/default.nix
@@ -37,9 +37,72 @@ in
     programs.niri.enable = true;
     programs.niri.package = pkgs.niri-unstable;
     home-manager.users.${mainUser}.programs.niri.settings = {
-      binds = {
-        "Mod+X".action.spawn = "ghostty";
+      prefer-no-csd = true;
+
+      input = {
+        focus-follows-mouse.enable = true;
       };
+
+      window-rules = [
+        {
+          geometry-corner-radius =
+            let
+              r = 10.0;
+            in
+            {
+              top-left = r;
+              top-right = r;
+              bottom-right = r;
+              bottom-left = r;
+            };
+
+          clip-to-geometry = true;
+        }
+      ];
+
+      layout = {
+        gaps = 10;
+
+        focus-ring = {
+          enable = true;
+          width = 3;
+          active.color = "#8aadf4";
+          inactive.color = "#ed8796";
+        };
+      };
+
+      spawn-at-startup = [
+        { sh = "vicinae server"; }
+        { sh = "quickshell"; }
+      ];
+
+      binds =
+        with config.lib.niri.actions;
+        let
+          sh = spawn "sh" "-c";
+        in
+        {
+          "Mod+Q".action = "close-window";
+          "Mod+Q".repeat = false;
+
+          "Mod+H".action = focus-column-left;
+          "Mod+J".action = focus-column-down;
+          "Mod+K".action = focus-column-up;
+          "Mod+L".action = focus-column-right;
+
+          "Mod+F".action = maximize-column;
+
+          "Print".action.screenshot = [ ];
+
+          "Mod+Escape".action.spawn = "wlogout";
+
+          # Apps
+          "Mod+X".action = spawn "ghostty";
+          "Mod+B".action = spawn "zen";
+          "Mod+E".action = spawn "nautilus";
+          "Mod+R".action = sh "~/.config/rofi/launchers/type-1/launcher.sh";
+          "Alt+Space".action = sh "vicinae toggle";
+        };
     };
   };
 }
