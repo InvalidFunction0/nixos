48016995436aff74d7dddb1993b59b3ba9969b37
Ayaan <ayaan.waqas@outlook.com>
2025-12-15 09:58:16 +0000

    basic niri config

--- modules/niri/default.nix
+++ modules/niri/default.nix
@@ -36,73 +36,93 @@ in
 
     programs.niri.enable = true;
     programs.niri.package = pkgs.niri-unstable;
-    home-manager.users.${mainUser}.programs.niri.settings = {
-      prefer-no-csd = true;
+    home-manager.users.${mainUser} =
+      { config, ... }:
+      {
+        programs.niri.settings = {
+          prefer-no-csd = true;
 
-      input = {
-        focus-follows-mouse.enable = true;
-      };
+          input = {
+            focus-follows-mouse.enable = true;
+            focus-follows-mouse.max-scroll-amount = "0%";
+          };
 
-      window-rules = [
-        {
-          geometry-corner-radius =
-            let
-              r = 10.0;
-            in
+          window-rules = [
             {
-              top-left = r;
-              top-right = r;
-              bottom-right = r;
-              bottom-left = r;
+              geometry-corner-radius =
+                let
+                  r = 10.0;
+                in
+                {
+                  top-left = r;
+                  top-right = r;
+                  bottom-right = r;
+                  bottom-left = r;
+                };
+
+              clip-to-geometry = true;
+            }
+          ];
+
+          layout = {
+            gaps = 10;
+
+            struts =
+              let
+                s = 16;
+              in
+              {
+                left = s;
+                right = s;
+              };
+
+            default-column-width = {
+              proportion = 1.0 / 2.0;
             };
 
-          clip-to-geometry = true;
-        }
-      ];
+            border = {
+              enable = true;
+              width = 3;
+              active.color = "#8aadf4";
+              inactive.color = "#ed8796";
+            };
 
-      layout = {
-        gaps = 10;
+            focus-ring.enable = false;
+          };
 
-        focus-ring = {
-          enable = true;
-          width = 3;
-          active.color = "#8aadf4";
-          inactive.color = "#ed8796";
+          spawn-at-startup = [
+            { sh = "vicinae server"; }
+            { sh = "quickshell"; }
+          ];
+
+          binds =
+            with config.lib.niri.actions;
+            let
+              sh = spawn "sh" "-c";
+            in
+            {
+              "Mod+Q".action = close-window;
+              "Mod+Q".repeat = false;
+
+              "Mod+H".action = focus-column-left;
+              "Mod+J".action = focus-window-down;
+              "Mod+K".action = focus-window-up;
+              "Mod+L".action = focus-column-right;
+
+              "Mod+F".action = maximize-column;
+
+              "Print".action.screenshot = [ ];
+
+              "Mod+Escape".action.spawn = "wlogout";
+
+              # Apps
+              "Mod+X".action = spawn "ghostty";
+              "Mod+B".action = spawn "zen";
+              "Mod+E".action = spawn "nautilus";
+              "Mod+R".action = sh "~/.config/rofi/launchers/type-1/launcher.sh";
+              "Alt+Space".action = sh "vicinae toggle";
+            };
         };
       };
-
-      spawn-at-startup = [
-        { sh = "vicinae server"; }
-        { sh = "quickshell"; }
-      ];
-
-      binds =
-        with config.lib.niri.actions;
-        let
-          sh = spawn "sh" "-c";
-        in
-        {
-          "Mod+Q".action = "close-window";
-          "Mod+Q".repeat = false;
-
-          "Mod+H".action = focus-column-left;
-          "Mod+J".action = focus-column-down;
-          "Mod+K".action = focus-column-up;
-          "Mod+L".action = focus-column-right;
-
-          "Mod+F".action = maximize-column;
-
-          "Print".action.screenshot = [ ];
-
-          "Mod+Escape".action.spawn = "wlogout";
-
-          # Apps
-          "Mod+X".action = spawn "ghostty";
-          "Mod+B".action = spawn "zen";
-          "Mod+E".action = spawn "nautilus";
-          "Mod+R".action = sh "~/.config/rofi/launchers/type-1/launcher.sh";
-          "Alt+Space".action = sh "vicinae toggle";
-        };
-    };
   };
 }
